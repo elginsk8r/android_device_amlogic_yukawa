62daf40e426fc9feaed0075438aea73c71f3f23b
Steve Pfetsch <spfetsch@google.com>
2016-07-11 12:26:23 -0700

    yukawa: Change OpenGL ES version to reflect 3.2 support
    Bug: 27347474
    Change-Id: Iee748ddc0522315757e33f5ca2be845f5f127267

--- device.mk
+++ device.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.egl=mali \
-    ro.opengles.version=196609
+    ro.opengles.version=196610
 
 # Vulkan
 PRODUCT_COPY_FILES += \
