b4a891bfe86f2c1ae33667d21958d02aa4acd6b8
Nick Reuter <nreuter85@gmail.com>
2021-09-18 19:04:48 -0500

    yukawa: add brand to the product information
    Change-Id: I216ce84208e2805986492eb88875faa0ce20d617

--- yukawa-common.mk
+++ yukawa-common.mk
@@ -1,6 +1,24 @@
 PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
-PRODUCT_BRAND := Android
+ifeq ($(TARGET_VIM3), true)
+PRODUCT_BRAND := Khadas
+else ifeq ($(TARGET_VIM3L), true)
+PRODUCT_BRAND := Khadas
+else
+PRODUCT_BRAND := SEI
+endif
+
+ifeq ($(TARGET_USE_TABLET_LAUNCHER), true)
+PRODUCT_MODEL := AOSP on yukawa
+else
 PRODUCT_MODEL := ATV on yukawa
+endif
+
+ifeq ($(TARGET_VIM3), true)
+PRODUCT_MANUFACTURER := Khadas
+else ifeq ($(TARGET_VIM3L), true)
+PRODUCT_MANUFACTURER := Khadas
+else
 PRODUCT_MANUFACTURER := SEI Robotics
+endif
