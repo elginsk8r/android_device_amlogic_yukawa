67d210182c207717c084683adbd98600d200d787
Nick Reuter <nreuter85@gmail.com>
2022-08-10 20:26:43 -0500

    yukawa: build kernel from source
    Change-Id: I53a0b52b5c99146f97ebf9d70cdb729d6c61d1eb

--- BoardConfigEvervolv.mk
+++ BoardConfigEvervolv.mk
@@ -8,7 +8,22 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml
 
 # Kernel
+BOARD_DTB_CFG := device/amlogic/yukawa/yukawa-dtb.cfg
+BOARD_DTBO_CFG := device/amlogic/yukawa/yukawa-dtbo.cfg
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
+BOARD_KERNEL_LZ4_COMPRESSION := true
+BOARD_KERNEL_LZ4_COMP_FLAGS := -f -12 --favor-decSpeed
+BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := meson_defconfig
+TARGET_KERNEL_DTB := \
+    amlogic/meson-g12a-sei510.dtb \
+    amlogic/meson-g12a-sei510-android.dtb \
+    amlogic/meson-sm1-sei610.dtb \
+    amlogic/meson-sm1-sei610-android.dtb \
+    amlogic/meson-sm1-khadas-vim3l.dtb \
+    amlogic/meson-sm1-khadas-vim3l-android.dtb \
+    amlogic/meson-g12b-a311d-khadas-vim3.dtb \
+    amlogic/meson-g12b-a311d-khadas-vim3-android.dtb \
+    amlogic/meson-g12b-odroid-n2.dtb \
+    amlogic/meson-g12b-odroid-n2-android.dtb
 TARGET_KERNEL_SOURCE := kernel/amlogic/yukawa
-TARGET_NO_KERNEL_OVERRIDE := true
