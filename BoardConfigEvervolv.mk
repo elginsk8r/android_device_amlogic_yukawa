d2c1f33abe1787848c2301c8b6aeee0e830ed4e1
Nick Reuter <nreuter85@gmail.com>
2022-08-10 20:26:43 -0500

    yukawa: build kernel from source
    Change-Id: I53a0b52b5c99146f97ebf9d70cdb729d6c61d1eb

--- BoardConfigEvervolv.mk
+++ BoardConfigEvervolv.mk
@@ -6,8 +6,19 @@
 TARGET_KERNEL_CONFIG := meson_defconfig
 TARGET_KERNEL_SOURCE := kernel/amlogic/yukawa
 
-# Kernel Image
-BOARD_KERNEL_IMAGE_NAME := Image.lz4
+# Kernel device tree
+BOARD_KERNEL_SEPARATED_DTBO ?= true
+BOARD_DTB_CFG := device/amlogic/yukawa/yukawa-dtb.cfg
+ifeq ($(BOARD_KERNEL_SEPARATED_DTBO), true)
+BOARD_DTBO_CFG := device/amlogic/yukawa/yukawa-dtbo.cfg
+endif
+
+# Kernel image
+BOARD_KERNEL_IMAGE_NAME ?= Image.lz4
+ifeq ($(BOARD_KERNEL_IMAGE_NAME), Image.lz4)
+BOARD_KERNEL_LZ4_COMPRESSION := true
+BOARD_KERNEL_LZ4_COMP_FLAGS := -f -12 --favor-decSpeed
+endif
 
 # Partitions
 -include $(SRC_EVERVOLV_DIR)/build/target/board/BoardConfigReservedSize.mk
