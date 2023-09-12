7ee0a2a144276afb5775e66b1339dc1c689028bf
Amit Pundir <amit.pundir@linaro.org>
2023-09-12 10:52:20 +0530

    yukawa: refactor graphics configs to independent fragments
    Breakdown graphics configs to project specific fragments and
    include them in target device config files individually.
    
    It is the first step in cherry-picking the board specific
    features/projects in the future target configuration(s).
    
    Change-Id: If4aec9b7c789a243b98fcce8b4a4b6fcfd828c0a
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

---
+++ shared/graphics/mali/device.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# GLES
+PRODUCT_PACKAGES += \
+    libGLES_android \
+    libGLES_mali
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610 \
+    ro.hardware.egl=mali
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2022-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
+
+# Gralloc
+PRODUCT_PACKAGES += \
+    gralloc.yukawa \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.gralloc=yukawa
+
+# Hardware composer
+PRODUCT_PACKAGES += \
+    android.hardware.composer.hwc3-service.drm.meson
+
+# Vulkan
+PRODUCT_PACKAGES += \
+    vulkan.yukawa.so
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.vulkan=yukawa
