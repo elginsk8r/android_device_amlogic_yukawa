5e3747dff13a8bad9e03059b3666f9e707111810
Amit Pundir <amit.pundir@linaro.org>
2023-10-15 21:05:34 +0530

    graphics: mali: Add ro.hardware.egl=mali
    A recent AOSP/main change has put a hard dependency on
    ro.hardware.egl or equivalent property being set, otherwise
    it fails to boot successfully:
    
    F DEBUG   : Cmdline: /system/bin/surfaceflinger
    F DEBUG   : pid: 672, tid: 677, name: surfaceflinger  >>> /system/bin/surfaceflinger <<<
    F DEBUG   : uid: 1000
    F DEBUG   : tagged_addr_ctrl: 0000000000000001 (PR_TAGGED_ADDR_ENABLE)
    F DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    F DEBUG   : Abort message: 'couldn't find an OpenGL ES implementation, make sure one of persist.graphics.egl, ro.hardware.egl and ro.board.platform is set'
    
    Change-Id: Ifa82dd3ea480f88b3c2646c6ca40a99d9ef52849
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- device-common.mk
+++ device-common.mk
@@ -172,7 +172,9 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320 \
+    ro.hardware.egl=mali
 
 PRODUCT_PACKAGES +=  libGLES_mali
 PRODUCT_PACKAGES +=  libGLES_android
