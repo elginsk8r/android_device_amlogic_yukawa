0f848c3b77d931cdabb0d6f6891c48235479bc3a
Guillaume La Roque <glaroque@baylibre.com>
2023-11-03 12:26:49 +0100

    yukawa: Remove deprecated platforms and dedicated 32-bit only support
    Change-Id: Ibafa849b34fa501e2e93d58231b0be6f3dda76cf

--- yukawa.mk
+++ yukawa.mk
@@ -1,14 +1,68 @@
 # Inherit the full_base and device configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, device/amlogic/yukawa/device-yukawa.mk)
-$(call inherit-product, device/amlogic/yukawa/yukawa-common.mk)
-
-PRODUCT_NAME := yukawa
-PRODUCT_DEVICE := yukawa
-
 ifndef TARGET_KERNEL_USE
 TARGET_KERNEL_USE := 5.10
 endif
+
+ifeq ($(TARGET_VIM3), true)
+TARGET_DEV_BOARD := vim3
+else ifeq ($(TARGET_VIM3L), true)
+TARGET_DEV_BOARD := vim3l
+else ifeq ($(TARGET_DEV_BOARD),)
+TARGET_DEV_BOARD := sei610
+endif
+
+ifneq ($(filter $(TARGET_DEV_BOARD),vim3),)
+AUDIO_DEFAULT_OUTPUT := hdmi
+GPU_TYPE := gondul_ion
+else ifneq ($(filter $(TARGET_DEV_BOARD),vim3l),)
+AUDIO_DEFAULT_OUTPUT := hdmi
+endif
+
+$(call inherit-product, device/amlogic/yukawa/device.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += ro.product.device=$(TARGET_DEV_BOARD)
+GPU_TYPE ?= dvalin_ion
+
+BOARD_KERNEL_DTB := device/amlogic/yukawa-kernel/$(TARGET_KERNEL_USE)
+
+ifeq ($(TARGET_PREBUILT_DTB),)
+LOCAL_DTB := $(BOARD_KERNEL_DTB)
+else
+LOCAL_DTB := $(TARGET_PREBUILT_DTB)
+endif
+
+# Feature permissions
+PRODUCT_COPY_FILES += \
+    device/amlogic/yukawa/permissions/yukawa.xml:/system/etc/sysconfig/yukawa.xml
+
+# Speaker EQ
+PRODUCT_COPY_FILES += \
+    device/amlogic/yukawa/hal/audio/speaker_eq_sei610.fir:$(TARGET_COPY_OUT_VENDOR)/etc/speaker_eq_sei610.fir
+
+# Hotword Mic Toggle Provider
+ifneq ($(filter $(TARGET_DEV_BOARD),sei610),)
+PRODUCT_PACKAGES += \
+    YukawaHotwordMicToggleProvider
+endif
+
+# Include namespaces for non-AB updater
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+AB_OTA_UPDATER := false
+
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
+ifeq ($(TARGET_USE_TABLET_LAUNCHER), true)
+PRODUCT_MODEL := Android Tablet on yukawa
+else
+PRODUCT_MODEL := ATV on yukawa
+endif
+
+PRODUCT_BRAND := Amlogic
+PRODUCT_MANUFACTURER := Amlogic
+PRODUCT_NAME := yukawa
+PRODUCT_DEVICE := yukawa
 
 MOD_DIR := device/amlogic/yukawa-kernel/$(TARGET_KERNEL_USE)
 
