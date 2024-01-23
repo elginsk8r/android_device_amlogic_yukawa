fa2710787e3166d580ca023202fb2f40213d9f84
Guillaume La Roque <glaroque@baylibre.com>
2024-01-23 13:35:22 +0100

    yukawa: Move init scripts out of init/hw
    Change-Id: I1f72d4ba77d85bb13a7538395af3491d8afece58

--- device.mk
+++ device.mk
@@ -98,8 +98,8 @@ endif
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.yukawa.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yukawa.rc \
-    $(LOCAL_PATH)/init.yukawa.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yukawa.usb.rc \
+    $(LOCAL_PATH)/init.yukawa.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.yukawa.rc \
+    $(LOCAL_PATH)/init.yukawa.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.yukawa.usb.rc \
     $(LOCAL_PATH)/init.recovery.hardware.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.yukawa.rc \
     $(LOCAL_PATH)/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
