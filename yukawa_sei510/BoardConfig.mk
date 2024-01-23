dabd4e86df509d738892204f96d5e02cd8548ad9
Guillaume La Roque <glaroque@baylibre.com>
2024-01-23 13:21:26 +0100

    yukawa: update partition size format
     Taken from "yukawa: manifest: update device manifest to level 8"
     and reduced to a single commit.
    
    Change-Id: Ib84ca4b45ef2fc081fe37f04d2cc74112983d089

--- yukawa_sei510/BoardConfig.mk
+++ yukawa_sei510/BoardConfig.mk
@@ -1,6 +1,6 @@
 include device/amlogic/yukawa/BoardConfigCommon.mk
 
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5054136320
+BOARD_USERDATAIMAGE_PARTITION_SIZE := $(shell echo $$(( 4820 * 1024 * 1024 )))
 
 TARGET_BOOTLOADER_BOARD_NAME := sei510
 TARGET_BOARD_INFO_FILE := device/amlogic/yukawa/board-info/board-info-sei510.txt
