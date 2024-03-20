c2ff46e095d0278e31a335329732891948355a4b
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- device.mk
+++ device.mk
@@ -11,9 +11,6 @@ PRODUCT_COPY_FILES +=  $(LOCAL_KERNEL):kernel
 # Build and run only ART
 PRODUCT_RUNTIMES := runtime_libart_default
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
