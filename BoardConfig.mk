ff3b405e44e8e80841a2773cd261e91a5514d981
Sumit Semwal <sumit.semwal@linaro.org>
2024-04-11 12:50:21 +0530

    yukawa: add 64-bit-only build flag TARGET_64BIT_ONLY
    TARGET_64BIT_ONLY will be set to false by default, so the default behaviour
    isn't changed. To build the userspace as 64bit-only, set it to true.
    
    Change-Id: Id7386a76927a15737d652481033a836ab715f632
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,12 +8,14 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a53
 
+ifeq ($(TARGET_64BIT_ONLY), false)
 # Secondary Arch
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
+endif
 
 # 64 bit mediadrmserver
 TARGET_ENABLE_MEDIADRM_64 := true
