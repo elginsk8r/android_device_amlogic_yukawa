6a113d7919d81d20a1bcbfc3d689fdbcd642ecde
Nick Reuter <nreuter85@gmail.com>
2024-11-28 18:13:39 -0600

    yukawa: add support for building gki kernel
    Change-Id: Ia41d7994eb38243dcb49feb16c7e70589a85906f

--- BoardConfigEvervolv.mk
+++ BoardConfigEvervolv.mk
@@ -3,8 +3,16 @@
 #
 
 # Kernel
-TARGET_KERNEL_CONFIG := meson_defconfig
 TARGET_KERNEL_SOURCE := kernel/amlogic/yukawa
+KERNEL_VERSION := $(shell grep -s "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+KERNEL_PATCHLEVEL := $(shell grep -s "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+TARGET_KERNEL_USE := $(KERNEL_VERSION).$(KERNEL_PATCHLEVEL)
+
+# Kernel config
+ifeq ($(TARGET_KERNEL_USE_GKI), true)
+TARGET_KERNEL_CONFIG := gki_defconfig amlogic_gki.config
+endif
+TARGET_KERNEL_CONFIG ?= meson_defconfig
 
 # Kernel device tree
 BOARD_KERNEL_SEPARATED_DTBO ?= true
@@ -18,6 +26,24 @@ BOARD_KERNEL_IMAGE_NAME ?= Image.lz4
 ifeq ($(BOARD_KERNEL_IMAGE_NAME), Image.lz4)
 BOARD_KERNEL_LZ4_COMPRESSION := true
 BOARD_KERNEL_LZ4_COMP_FLAGS := -f -12 --favor-decSpeed
+endif
+
+# Kernel modules
+ifeq ($(TARGET_KERNEL_USE_GKI), true)
+BOARD_KERNEL_MODULES_LOAD := \
+    device/amlogic/yukawa/modules.load-$(TARGET_KERNEL_USE) \
+    $(if $(TARGET_USES_NANOHUB_SENSORHAL),device/amlogic/yukawa/modules.load.nanohub,)
+BOARD_KERNEL_MODULES_LOAD := $(strip $(shell cat $(BOARD_KERNEL_MODULES_LOAD)))
+BOARD_RECOVERY_KERNEL_MODULES_LOAD := $(BOARD_KERNEL_MODULES_LOAD)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(BOARD_KERNEL_MODULES_LOAD)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(BOARD_VENDOR_KERNEL_MODULES_LOAD)
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
+RECOVERY_KERNEL_MODULES := $(BOARD_RECOVERY_KERNEL_MODULES_LOAD)
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/google-modules
+TARGET_KERNEL_EXT_MODULES := gpu
+ifeq ($(TARGET_USES_NANOHUB_SENSORHAL), true)
+TARGET_KERNEL_EXT_MODULES += nanohub
+endif
 endif
 
 # Partitions
