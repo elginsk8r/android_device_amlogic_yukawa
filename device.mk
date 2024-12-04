886b9714f3b4b996d25d6abe2db42a715457e6dd
Yongqin Liu <yongqin.liu@linaro.org>
2024-12-04 11:12:56 +0800

    yukawa: update the gatekeeper HAL
    with the APEX and AIDL version, to prepare
    for the API LEVEL 34 upgrade.
    
    Referred to the goldfish change here:
        https://android-review.googlesource.com/c/device/generic/goldfish/+/3316024
    
    Test: atest vts_treble_vintf_vendor_test
    Test: VtsHalGatekeeperTargetTest
    
    Change-Id: I0974ea818433c0ee5153546de7cbe55825ee9235
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- device.mk
+++ device.mk
@@ -349,7 +349,7 @@ endif
 
 # Software Security HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service.software
+    com.android.hardware.gatekeeper.nonsecure
 
 # KeyMint
 PRODUCT_PACKAGES += \
