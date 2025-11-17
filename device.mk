ad06d1d21a903f2f0e10d57c21192808b3570846
Guillaume La Roque <glaroque@baylibre.com>
2025-11-17 21:56:04 +0100

    yukawa: refactor sensor HAL configuration into separate module
    Move sensor HAL configuration from device.mk to a dedicated
    hal/sensorhal/device_vendor.mk file for better organization.
    
    Changes:
    - Move sensor manifest and implementation files to hal/sensorhal/
    - Consolidate nanohub sensor HAL configuration in dedicated module
    - Preserve conditional sensor configuration based on TARGET_SENSOR_MEZZANINE
    
    Change-Id: Ifdd43e71328458d7beffc4ea151f6ad89f88593d
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- device.mk
+++ device.mk
@@ -249,48 +249,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.google.cf.health.storage
 
-# Sensor HAL
-ifneq ($(TARGET_SENSOR_MEZZANINE),)
-TARGET_USES_NANOHUB_SENSORHAL := true
-NANOHUB_SENSORHAL_LID_STATE_ENABLED := true
-NANOHUB_SENSORHAL_SENSORLIST := $(LOCAL_PATH)/sensorhal/sensorlist_$(TARGET_SENSOR_MEZZANINE).cpp
-NANOHUB_SENSORHAL_DIRECT_REPORT_ENABLED := true
-NANOHUB_SENSORHAL_DYNAMIC_SENSOR_EXT_ENABLED := true
-
-PRODUCT_PACKAGES += \
-    context_hub.default \
-    sensors.yukawa \
-    android.hardware.sensors@1.0-service \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.contexthub@1.2-service \
-    android.hardware.contexthub@1.2-impl
-
-# Nanohub tools
-PRODUCT_PACKAGES += stm32_flash nanoapp_cmd nanotool
-
-PRODUCT_COPY_FILES += \
-    device/amlogic/yukawa/init.common.nanohub.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nanohub.rc
-
-# Copy sensors config file(s)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.relative_humidity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.relative_humidity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
-
-# Argonkey VL53L0X proximity driver is not available yet. So we are going to copy conf file for neonkey only
-ifeq ($(TARGET_SENSOR_MEZZANINE),neonkey)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml
-endif
-endif
-
 #
 # Authsecret AIDL HAL
 #
@@ -360,3 +318,6 @@ $(call inherit-product, device/amlogic/yukawa/hal/graphics/device_vendor.mk)
 
 # Connectivity
 $(call inherit-product, device/amlogic/yukawa/hal/connectivity/device_vendor.mk)
+
+# Sensor HAL
+$(call inherit-product, device/amlogic/yukawa/hal/sensorhal/device_vendor.mk)
