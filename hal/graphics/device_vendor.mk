a8b835d92e18c0499f8d05680b70d8472a291f9f
Guillaume La Roque <glaroque@baylibre.com>
2025-11-17 22:45:43 +0100

    yukawa: migrate graphics stack from Mali to Mesa
    Replace Mali proprietary graphics stack with Mesa open-source
    implementation using minigbm allocator and DRM HWC.
    
    Key changes:
    - Replace Mali gralloc with minigbm allocator/mapper
    - Switch from Mali EGL/Vulkan to Mesa implementations
    - Migrate HWC from Meson-specific to generic DRM HWC3 service
    - Update Vulkan support from 1.1 to 1.0.3 (Mesa capability)
    - Add OpenGL ES AEP and DEQP level support
    - Add Vulkan DEQP level support
    - Configure board-specific Mesa paths (A55 for vim3l, A73 for vim3)
    - Add GBM backend configuration for Mesa
    - Set LCD density to 160
    - Update OpenGL ES version to 3.2 (196864)
    - Add display settings configuration
    - Use Skia GL threaded render engine backend
    
    Change-Id: Ic921c87c7cf77e051b97d35cdfe8275153fea0b2
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- hal/graphics/device_vendor.mk
+++ hal/graphics/device_vendor.mk
@@ -1,32 +1,56 @@
 # Graphics #
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
+ifeq ($(TARGET_DEV_BOARD), vim3l)
+PRODUCT_SOONG_NAMESPACES += vendor/amlogic/yukawa/gpu/mesa/a55
+else
+PRODUCT_SOONG_NAMESPACES += vendor/amlogic/yukawa/gpu/mesa/a73
+endif
+PRODUCT_VENDOR_PROPERTIES += ro.sf.lcd_density=160
+# HWUI VULKAN not working with mesa 25.3
+# TARGET_USES_VULKAN = true
+
+# opengles features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
 # Vulkan
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:vendor/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:vendor/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:vendor/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:vendor/etc/permissions/android.hardware.vulkan.level.xml
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:vendor/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:vendor/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Minigbm mapper/allocator
 PRODUCT_PACKAGES += \
-    gralloc.yukawa \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.allocator-service.minigbm \
+    gralloc.minigbm \
+    libminigbm_gralloc \
+    mapper.minigbm \
+    libgbm_mesa_wrapper
+
+# Mesa GBM backend path
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.hwcomposer=drm \
+    ro.hardware.egl=mesa \
+    ro.hardware.vulkan=mesa \
+    graphics.gpu.profiler.support=true \
+    vendor.hwc.drm.device=/dev/dri/card1 \
+    ro.hardware.gralloc=minigbm \
+    vendor.gralloc.minigbm.backend=gbm_mesa \
+    vendor.mesa.gbm_backends_path=/vendor/lib64/gbm \
+    debug.renderengine.backend=skiaglthreaded
 
 # Hardware Composer HAL
 #
-PRODUCT_PACKAGES += android.hardware.composer.hwc3-service.drm.meson
+PRODUCT_PACKAGES += android.hardware.composer.hwc3-service.drm
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.egl=mali \
-    ro.hardware.vulkan=yukawa
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.gralloc=yukawa
+# Display settings (windowing, system decorations, IME ...)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml
 
 # Create input surface on the framework side
 PRODUCT_VENDOR_PROPERTIES += \
     debug.stagefright.c2inputsurface=-1
 
 PRODUCT_VENDOR_PROPERTIES += \
-	ro.opengles.version=196610
+	ro.opengles.version=196864
