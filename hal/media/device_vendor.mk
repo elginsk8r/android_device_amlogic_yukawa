c78956f26e49d4f3f4d1a40bf3eaf4ca60e038a2
Guillaume La Roque <glaroque@baylibre.com>
2025-11-20 20:13:25 +0100

    yukawa: refactor media configuration into separate HAL module
    Move media codec profiles and configuration from device-level to a
    dedicated HAL module under hal/media/.
    
    This refactoring:
    - Creates hal/media/device_vendor.mk for media HAL integration
    - Moves media_profiles.xml to hal/media/ directory
    - Adds product.prop for media-specific properties
    - Removes obsolete top-level media_xml/ directory
    - Removes hardcoded media codec XML file copies from device.mk
    
    The new structure allows better organization and makes the media
    configuration self-contained and easier to maintain.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

---
+++ hal/media/device_vendor.mk
@@ -0,0 +1,9 @@
+TARGET_PRODUCT_PROP += \
+	device/amlogic/yukawa/hal/media/product.prop
+
+# Copy media codecs config file
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    device/amlogic/yukawa/hal/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
